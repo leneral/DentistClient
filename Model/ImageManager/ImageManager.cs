78b3e2cf904e50738bfb826a2092dc90ca73944f
leneral <leneral@126.com>
2013-03-07 16:34:51 +0800

    add ImageManager GetImage Function

--- Model/ImageManager/ImageManager.cs
+++ Model/ImageManager/ImageManager.cs
@@ -23,6 +23,16 @@ namespace Model.ImageManager
                 }
             }
             return photoByte;
-        }   
+        }
+        public static Image GetImage(byte[] photobyte)
+        {
+            Image img = null;
+            if (photobyte.Length != 0)
+            {
+                var ms = new System.IO.MemoryStream(photobyte);
+                img = Image.FromStream(ms);
+            }
+            return img;
+        }
     }
 }
